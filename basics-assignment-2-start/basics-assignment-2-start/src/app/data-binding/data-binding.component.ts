f7acd270d16a952d1d0586aff9ad8d20aece48c7
Sam Cherian <samnp87@gmail.com>
2019-03-26 23:29:40 +0530

    Value cahnge in ts file

--- basics-assignment-2-start/basics-assignment-2-start/src/app/data-binding/data-binding.component.ts
+++ basics-assignment-2-start/basics-assignment-2-start/src/app/data-binding/data-binding.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.css']
 })
 export class DataBindingComponent implements OnInit {
-  username = '';
+  username:any;
 
   constructor() {
 
