7296b80da0acf6a977ea96ec30fe6a302cedb23c
Sam Cherian <samnp87@gmail.com>
2019-03-28 22:47:31 +0530

    Completed Exercise 3 on Directives

--- basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
@@ -1,28 +1,17 @@
-import { Component } from '@angular/core';
-import { callLifecycleHooksChildrenFirst } from '@angular/core/src/view/provider';
+import { Component } from "@angular/core";
+import { callLifecycleHooksChildrenFirst } from "@angular/core/src/view/provider";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-// this is setting the value for this variable for the first time
-para = true;
-buttonLog = 0;
-buttonlogArray = [];
-
-paraToggle() {
-  // This acts like a switch
-  // First time when you click
-  // it turns this.para value it into(!this.para) false
-  // again when clicked since the value is false !this.para will change this.para value to true
-  // so acting like a switch
-  this.para = !this.para;
-  console.log(this.para);
-  this.buttonlogArray.push(this.buttonLog++);
-}
-
-
-
+  hideP = true;
+  logs = [];
+  increment = 1;
+  hideDetails() {
+    this.hideP = !this.hideP;
+    this.logs.push(this.increment++);
+  }
 }
